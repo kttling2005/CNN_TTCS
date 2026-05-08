aed5d5737ff513eaaeba57f9a24ba0b34c372b29
kttling2005 <80590767+kttling2005@users.noreply.github.com>
2026-05-08 23:19:26 +0700

    Refactor MobileNetV2 model training and saving
    Updated the MobileNetV2 model implementation to include data preprocessing and model training steps. Added model saving functionality for the new model version.

--- create_mobilenet_model.py
+++ create_mobilenet_model.py
@@ -1,7 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.layers.normalization import normalization
+from tensorflow.keras import layers
+from tensorflow.keras import models
 from tensorflow.keras.applications import MobileNetV2
 
-base_model = MobileNetV2(weights='imagenet', include_top=False)
+#chuan hoa du lieu vi model MovbileNetV2 chay tren dai gia tri [-1,1]
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
-history2 = model.fit(train_data, validation_data=val_data, epochs=10)
+train_data = train_data.map(lambda x, y: (preprocess_input(x), y))
+val_data = val_data.map(lambda x, y: (preprocess_input(x), y))
+
+base_model = MobileNetV2(input_shape=(150, 150, 3), weights='imagenet', include_top=False)
+
+# 2. Đóng băng base model để giữ lại kiến thức cũ
+base_model.trainable = False
+
+model = models.Sequential([
+    base_model,
+    layers.GlobalAveragePooling2D(), # Chuyển tensor 4D về 2D
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(38, activation='softmax') # n_classes là số lớp của bạn
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=10
+)
+
+model.save("plant_disease_model_mobileNetV2_v2.keras")
 
 model.save("plant_disease_model_mobilenet.keras")
